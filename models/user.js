dce4d7d0faeb4be96f66986d9b8a6955c0b93d65
Mihail Martynyuk <mishanudpsu@gmail.com>
2016-06-08 16:10:18 +0300

    add

--- models/user.js
+++ models/user.js
@@ -1,11 +1,17 @@
-const mongoose = require('mongoose')
-    , bcrypt   = require('bcrypt-nodejs');
+const  mongoose = require('mongoose')
+     , bcrypt   = require('bcrypt-nodejs');
 
 var userSchema = mongoose.Schema({
     local: {
-        name: String,
-        email: String,
-        password: String
+        email: {
+            type: String,
+            require: true,
+            unique: true
+        },
+        password: {
+            type: String,
+            require: true
+        }
     }
 });
 
